43d67d05b0bcdf74d466cbafbb7742aba7cafb65
mike <mike@aolccbc.com>
2023-08-01 10:16:47 -0700

    New scripts

--- Fix-Folder-names.ps1
+++ Fix-Folder-names.ps1
@@ -8,6 +8,7 @@ foreach ($folder in $folders ) {
 $newName =  $folder.Name.Replace(" ","_").Replace("-","").Replace("__","_").Replace("Instructions.pdf","Kit")
 $root = ($folder.FullName).Replace($folder.Name,"").Replace("Practical_Exam_Kit\","")
 Write-Host $root should be $newName
+Rename-Item -Path "$root\Practical_Exam_Kit" -NewName $newName
 #$dest = (Split-Path -Path $folder.FullName -Parent) + "\Practical_Exam_Kit.zip"
 #Compress-Archive -Path $folder.FullName -DestinationPath $dest -force
 }
